ea034d7a0be2616c896f0384881cdec5769d3645
Serhii Hrabas <graser1305@gmail.com>
2013-11-18 21:35:16 +0200

    fixed small bug, changed time picking layout, days are saving as a string, e.g. mon,tue,wed,thu

--- src/com/example/ringcon/AddRuleFragment.java
+++ src/com/example/ringcon/AddRuleFragment.java
@@ -20,6 +20,7 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TimePicker;
+import android.widget.ToggleButton;
 
 import com.example.ringcon.sql.DBHelper;
 
@@ -29,6 +30,15 @@ public class AddRuleFragment extends DialogFragment {
 	DBHelper mDbHelper;
 	TimePicker startTimePicker;
 	TimePicker endTimePicker;
+	
+	ToggleButton monTb;
+	ToggleButton tueTb;
+	ToggleButton wedTb;
+	ToggleButton thuTb;
+	ToggleButton friTb;
+	ToggleButton satTb;
+	ToggleButton sunTb;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 //		setRetainInstance(true);
@@ -44,27 +54,52 @@ public class AddRuleFragment extends DialogFragment {
 			Bundle savedInstanceState) {
 
 		containerView = (ScrollView) inflater.inflate(R.layout.dailog_add_rule, null);
+		
 		startTimePicker = (TimePicker) containerView.findViewById(R.id.startTime);
 		endTimePicker = (TimePicker) containerView.findViewById(R.id.endTime);
+		
+		monTb = (ToggleButton) containerView.findViewById(R.id.tbtnMon);
+		tueTb = (ToggleButton) containerView.findViewById(R.id.tbtnTue);
+		wedTb = (ToggleButton) containerView.findViewById(R.id.tbtnWed);
+		thuTb = (ToggleButton) containerView.findViewById(R.id.tbtnThu);
+		friTb = (ToggleButton) containerView.findViewById(R.id.tbtnFri);
+		satTb = (ToggleButton) containerView.findViewById(R.id.tbtnSat);
+		sunTb = (ToggleButton) containerView.findViewById(R.id.tbtnSun);
+		
+		startTimePicker.setIs24HourView(true);
+		endTimePicker.setIs24HourView(true);
 
 		((Button) containerView.findViewById(R.id.addRule)).setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-
-//				Calendar startCalendar = new GregorianCalendar(0, 0, 0, startTimePicker.getCurrentHour(), startTimePicker.getCurrentMinute());
-//				Calendar endCalendar = new GregorianCalendar(0, 0, 0, endTimePicker.getCurrentHour(), endTimePicker.getCurrentMinute());
-				
+				String days = "";
+				if(monTb.isChecked())
+					days += "mon,";
+				if(tueTb.isChecked())
+					days += "tue,";
+				if(wedTb.isChecked())
+					days += "wed,";
+				if(thuTb.isChecked())
+					days += "thu,";
+				if(friTb.isChecked())
+					days += "fri,";
+				if(satTb.isChecked())
+					days += "sat,";
+				if(sunTb.isChecked())
+					days += "sun,";
+				if(days.length()>0)
+					days = days.substring(0, days.length()-1);
+				Log.d("DAYS", days);
 				SQLiteDatabase db = mDbHelper.getWritableDatabase();
 				
 				SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm"); 
 				Date startDate = new Date(0, 0, 0, startTimePicker.getCurrentHour(), startTimePicker.getCurrentMinute());
-				Date endDate = new Date(0, 0, 0, startTimePicker.getCurrentHour(), endTimePicker.getCurrentMinute());
+				Date endDate = new Date(0, 0, 0, endTimePicker.getCurrentHour(), endTimePicker.getCurrentMinute());
 				ContentValues values = new ContentValues();
-				Log.d("END DATE", dateFormat.format(endDate));
 				values.put(Rule.KEY_STARTDATE, dateFormat.format(startDate));
 				values.put(Rule.KEY_ENDDATE, dateFormat.format(endDate));
 				values.put(Rule.KEY_ACTIVE, true);
-				values.put(Rule.KEY_WEEKDAYS, "smth");
+				values.put(Rule.KEY_WEEKDAYS, days);
 				
 				db.insert(DBHelper.TABLE_NAME, null, values);
 			}
