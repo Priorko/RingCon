0fa7cbbf140734af8c7d9eed6ec489e8029c537f
Igor Flysta <iflista@gmail.com>
2013-11-18 22:45:09 +0200

    Added edit layout

--- src/com/example/ringcon/Main.java
+++ src/com/example/ringcon/Main.java
@@ -12,34 +12,4 @@ import android.widget.ListView;
 
 public class Main extends FragmentActivity {
 
-	Rule[] rules ={new Rule(new Date(), new Date(), true),new Rule(new Date(), new Date(), true),new Rule(new Date(), new Date(), true),};
-	ListView ruleList;
-	RulesAdapter ruleAdapter;
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		
-		
-		ArrayList<Rule> assetList = new ArrayList<Rule>(Arrays.asList(rules));
-		ruleAdapter = new RulesAdapter(this, assetList);
-
-		ruleList = (ListView) findViewById(R.id.ruleList);
-		ruleList.setAdapter(ruleAdapter);
-
-	}
-
-	public void onAddRule(View v) {
-		AddRuleFragment mContent = new AddRuleFragment();
-		mContent.show(getSupportFragmentManager(), "add_rule");
-	}
-	// place the menu items into the action bar
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-	    // Inflate the menu items for use in the action bar
-	    MenuInflater inflater = getMenuInflater();
-	    inflater.inflate(R.menu.main_activity_actions, menu);
-	    return super.onCreateOptionsMenu(menu);
-	}
 }
