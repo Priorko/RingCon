33b604eb49d38b3e57350fc0695b1ba69e2da07b
Serhii Hrabas <graser1305@gmail.com>
2014-03-20 12:59:07 +0200

    changed styles

--- src/com/example/ringcon/MainActivity.java
+++ src/com/example/ringcon/MainActivity.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
